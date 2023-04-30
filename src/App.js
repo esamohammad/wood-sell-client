2f3eca65c086fbfe9eb9c394537ed8c0560d3a1f
esamohammad <esa.ce2020@gmail.com>
2023-04-30 16:14:49 +0600

    loader set

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import { RouterProvider } from 'react-router-dom'
 import { routes } from './routes/Routes';
+import { useEffect, useState } from "react";
 import './App.css';
+import Loader from './utils/Loader';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2500);
+  });
+
   return (
-    <div className="App">
+    <div>
+      <div>
+        {isLoading ? (
+          <Loader></Loader>
+        ) : (
+          <div>
+            <div className="mx-auto max-w-full">
       <RouterProvider router={routes}></RouterProvider>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
