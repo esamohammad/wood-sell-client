b97bc9b2758d6714a21832f0b1f1b8a0c41128d6
esamohammad <esa.ce2020@gmail.com>
2023-05-22 19:59:48 +0600

    Form Make -with showing data in console

--- src/pages/Dashboard/AddProduct/AddProduct.js
+++ src/pages/Dashboard/AddProduct/AddProduct.js
@@ -1,9 +1,76 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import Spinner from '../../../utils/Spinner';
 
 
+
+
+//*=====================================\\
+//!Condition of product array.
+const conditions = [
+    {
+        id: 1,
+        name: 'Good'
+    },
+    {
+        id: 2,
+        name: 'Excellent'
+    },
+    {
+        id: 3,
+        name: 'Fair'
+    },
+    {
+        id: 4,
+        name: 'Average'
+    },
+    {
+        id: 5,
+        name: 'Below Average'
+    },
+    {
+        id: 6,
+        name: 'Poor Quality'
+    },
+]
+//*=====================================\\
+
+
+
+
+
+
+
+
+
+
+//*=====================================\\
+//!M.component
 const AddProduct = () => {
     const { register, handleSubmit, formState: { errors } } = useForm();
+
+
+
+
+
+
+
+
+    //*=====================================\\
+    // !react quary -category name project.
+    const { data: categoryNames, isLoading } = useQuery({
+        queryKey: ['categoryName'],
+        queryFn: async () => {
+            const res = await fetch('http://localhost:5000/categoryName');
+            const data = await res.json();
+            return data;
+        }
+    })
+
+
+
+
 
 
 
@@ -15,11 +82,30 @@ const AddProduct = () => {
 
 
 
+
+
+
+
     //*=====================================\\
-    //!Current Date Variable
+    //!Current Date Variable(Today Date)
     let today = new Date();
     let date = today.getDate() + "-" + parseInt(today.getMonth() + 1) + "-" + today.getFullYear();
     // console.log(date)
+
+
+
+
+
+
+    //*=====================================\\
+    //!react quary data loading spinner.
+    if (isLoading) {
+        return <Spinner></Spinner>
+    }
+
+
+
+
 
 
 
@@ -30,8 +116,13 @@ const AddProduct = () => {
                 <form onSubmit={handleSubmit(handleAddDoctor)}>
 
 
+
+
                     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
-                        <div className="form-control w-full max-w-xs">
+
+
+                        {/* //!Product Name */}
+                        <div className="form-control w-full max-w-xs mx-auto">
                             <label className="label"> <span className="label-text font-bold">Product Name</span></label>
                             <input type="text" {...register("productName", {
                                 required: "Product Name is Required"
@@ -40,7 +131,9 @@ const AddProduct = () => {
                         </div>
 
 
-                        <div className="form-control w-full max-w-xs">
+
+                        {/* //!Location */}
+                        <div className="form-control w-full max-w-xs mx-auto">
                             <label className="label"> <span className="label-text font-bold">Location</span></label>
                             <input type="text" {...register("location", {
                                 required: "Location is Required"
@@ -49,7 +142,9 @@ const AddProduct = () => {
                         </div>
 
 
-                        <div className="form-control w-full max-w-xs">
+
+
+                        <div className="form-control w-full max-w-xs mx-auto">
                             <label className="label"> <span className="label-text font-bold">Resell Price</span></label>
                             <input type="number" {...register("resellPrice", {
                                 required: "Resell Price is Required"
@@ -58,7 +153,9 @@ const AddProduct = () => {
                         </div>
 
 
-                        <div className="form-control w-full max-w-xs">
+
+
+                        <div className="form-control w-full max-w-xs mx-auto">
                             <label className="label"> <span className="label-text font-bold">Original Price</span></label>
                             <input type="number" {...register("originalPrice", {
                                 required: "Original Price is Required"
@@ -67,7 +164,9 @@ const AddProduct = () => {
                         </div>
 
 
-                        <div className="form-control w-full max-w-xs">
+
+
+                        <div className="form-control w-full max-w-xs mx-auto">
                             <label className="label"> <span className="label-text font-bold">Used Year</span></label>
                             <input type="number" {...register("usedYear", {
                                 required: "Used Year is Required"
@@ -75,15 +174,26 @@ const AddProduct = () => {
                             {errors.usedYear && <p className='text-red-500 text-sm'>{errors.usedYear.message}</p>}
                         </div>
 
-                        <div className="form-control w-full max-w-xs">
-                            <label className="label"> <span className="label-text font-bold">Condition</span></label>
-                            <input type="text" {...register("condition", {
-                                required: "Condition is Required"
-                            })} className="input input-bordered w-full max-w-xs input-sm input-primary" />
-                            {errors.condition && <p className='text-red-500 text-sm'>{errors.condition.message}</p>}
+
+
+                        {/* //!select condition. */}
+                        <div className="form-control w-full max-w-xs mx-auto">
+                            <label className="label"> <span className="label-text font-bold">Select Condition</span> </label>
+                            <select   {...register('condition')} className="select select-sm select-primary w-full max-w-xs ">
+                                {/* <option disabled selected>Dark mode or light mode?</option> */}
+                                {
+                                    conditions.map(condition => <option
+                                        key={condition.id}
+                                        value={condition.name}
+                                    >{condition.name}</option>)
+                                }
+
+                            </select>
                         </div>
 
-                        <div className="form-control w-full max-w-xs">
+
+
+                        <div className="form-control w-full max-w-xs mx-auto">
                             <label className="label"> <span className="label-text font-bold">Seller Name</span></label>
                             <input type="text" {...register("sellerName", {
                                 required: "Seller Name is Required"
@@ -91,25 +201,50 @@ const AddProduct = () => {
                             {errors.sellerName && <p className='text-red-500 text-sm'>{errors.sellerName.message}</p>}
                         </div>
 
-                        <div className="form-control w-full max-w-xs">
+
+                        {/* {//!Product Posting Date} */}
+                        <div className="form-control w-full max-w-xs mx-auto">
                             <label className="label"> <span className="label-text font-bold">Product Posting Date</span></label>
                             <input value={`${date}`} fix type="text" {...register("postingDate", {
                                 required: "Filled Automatically"
                             })} className="input input-bordered w-full max-w-xs input-sm input-primary" />
                             {errors.postingDate && <p className='text-red-500 text-sm'>{errors.postingDate.message}</p>}
                         </div>
+
+
+
+
+
+                        {/* //!select category. */}
+                        <div className="form-control w-full max-w-xs mx-auto">
+                            <label className="label"> <span className="label-text font-bold">Select Category</span> </label>
+                            <select   {...register('categoryName')} className="select select-sm select-primary w-full max-w-xs ">
+                                {
+                                    categoryNames.map(categoryName => <option
+                                        key={categoryName._id}
+                                        value={categoryName.name}
+                                    >{categoryName.name}</option>)
+                                }
+
+                            </select>
+                        </div>
+
+
                     </div>
 
-                    <div className="form-control w-full max-w-xs">
-                        <label className="label"> <span className="label-text font-bold">Select Condition</span></label>
-                        <select className="select select-primary w-full max-w-xs input-sm">
-                            <option disabled selected>Condition of your product?</option>
-                            <option>Auto</option>
-                            <option>Dark mode</option>
-                            <option>Light mode</option>
-                        </select>
+                    <div>
+
+                        {/* //!Product Name */}
+                        <div className="form-control w-full mt-5 grid grid-cols-1 mx-auto">
+                            <label className="label"> <span className="label-text font-bold">Upload Product Image</span></label>
+                            <input type="file" {...register("productImage", {
+                                required: "Product Image is Required"
+                            })} className="file-input file-input-bordered file-input-success max-w-full "/>
+                            {errors.productImage && <p className='text-red-500 text-sm'>{errors.productImage.message}</p>}
+                        </div>
                     </div>
-                    <input className='btn btn-outline  w-full mt-4 ' value="Add Product" type="submit" />
+
+                    <input className='btn btn-outline  w-full mt-4 mx-auto' value="Add Product" type="submit" />
                 </form>
             </div>
         </div>
