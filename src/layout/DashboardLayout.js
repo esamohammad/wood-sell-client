820a4fe4817362a48124eb96199b5b7b283295a4
esamohammad <esa.ce2020@gmail.com>
2023-05-23 16:28:56 +0600

    BUG FIX- BG-Color Blinking when modal open

--- src/layout/DashboardLayout.js
+++ src/layout/DashboardLayout.js
@@ -22,7 +22,7 @@ const DashboardLayout = () => {
                 </div>
                 <div className="drawer-side">
                     <label htmlFor="dashboard-drawer" className="drawer-overlay"></label>
-                    <ul className="menu p-4 w-80 bg-base-100 text-base-content">
+                    <ul className="menu p-4 w-80  text-base-content">
                         <li className=' text-lg font-bold text-secondary-focus mb-0.5'><Link to='/dashboard'> <span className='text-indigo-600 '><FaTasks /></span> My Orders </Link></li>
                         {
                             isAdmin && <>
